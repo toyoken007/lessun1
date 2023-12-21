ddd343323aedc75071d9b90a2ab3eef20102f895
toyoken3308 <toyoken007@gmail.com>
2023-12-21 23:08:57 +0900

    no message

--- js/ham.js
+++ js/ham.js
@@ -1,9 +1,24 @@
-$(".openbtn").click(function () {
-	$(this).toggleClass('active');
-	$(".nav").toggleClass('active');
-});
+$(function () {
+    // $(".openbtn").click(function () {
+    //     $(this).toggleClass('active');
+    //     $(".nav").toggleClass('active');
+    // });
 
-$('a[href*="#"]').on('click', function () {
-	$('#js-hamburger').removeClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-  $('#js-nav').removeClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
-});
+    // $('a[href*="#"]').on('click', function () {
+    //     $('#js-hamburger').removeClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+    //   $('#js-nav').removeClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+    // });
+
+    const ham = $('#js-hamburger');
+    const nav = $('#js_nav');
+    ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+        ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+        nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外
+    })
+
+    // const ham = $('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+    // const nav = $('#js-nav'); //js-navの要素を取得し、変数navに格納
+    // ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+    //     console.log("ok!"); //コンソール画面でokというメッセージが出る
+    // })
+})
