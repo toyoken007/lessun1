9cf4dd403dd86c9c8b5579b7a9a3edb41e577f97
toyoken3308 <toyoken007@gmail.com>
2023-12-24 23:10:36 +0900

    no message

--- js/ham.js
+++ js/ham.js
@@ -16,6 +16,10 @@ $(function () {
         nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外
     })
 
+    // $("#js-hamburger").on('click', function () { //ハンバーガーメニューをクリックしたら
+    //     $(".nav").fadeToggle(1000);
+    // })
+
     // const ham = $('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
     // const nav = $('#js-nav'); //js-navの要素を取得し、変数navに格納
     // ham.on('click', function () { //ハンバーガーメニューをクリックしたら
